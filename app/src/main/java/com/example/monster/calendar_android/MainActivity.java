c761c44e647ed85a4acacfc72d5e35f8c1d93d6a
unknown <emeldincc@gmail.com>
2018-08-05 01:53:57 +0300

    added Calendar View

--- app/src/main/java/com/example/monster/calendar_android/MainActivity.java
+++ app/src/main/java/com/example/monster/calendar_android/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.monster.calendar_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView  myDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1) + "/" + i2 +"/" + i;
+                myDate.setText(date);
+            }
+        });
+
+
+
     }
+
+
 }
